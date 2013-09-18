ae0c3e20dfd04f05afe95ccba1191d5ce1d2abb5
Andrey Kuznetsov <ferus.tigris@gmail.com>
2013-09-18 13:40:02 -0400

    Unused code has been removed

--- assets/www/js/objects/bonus.js
+++ assets/www/js/objects/bonus.js
@@ -4,15 +4,8 @@ Crafty.c('Bonus', {
     this.requires("Canvas");
     this.requires("repa");
     this.requires("bonus");
-    this.requires("SpriteAnimation");
 
     this.attr({x: 0, y: 0});
-    this.animate("wind", 0, 0, 0);
-
-    this.bind("EnterFrame", function() {
-      if(!this.isPlaying());
-        this.animate("wind", 80);
-    });
   },
 
   clear: function() {
