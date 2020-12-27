3fbcca9c15de6e706b3a9dcccdd9ed53bea3bd9f
Andrey_Kuznetsov <Andrey_Kuznetsov@epam.com>
2020-12-27 10:42:06 +0100

    Refactoring

--- assets/www/js/game.js
+++ assets/www/js/game.js
@@ -86,114 +86,65 @@ require(AllScripts, function() {
 
     Game.level = new Level(Settings);
 
-    // подгружаем спрайт
-    Crafty.sprite(Settings.poligon, "images/digger.png", {
-        digger: [0,0]
-    });
-    // подгружаем спрайт
-    Crafty.sprite(Settings.poligon, "images/monster.png", {
-        monster: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/target1.png", {
-        target1: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/tracktarget_completed.png", {
-        tracktargetcompleted: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/target1_completed.png", {
-        target1completed: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/soundon.png", {
-        soundon: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/soundoff.png", {
-        soundoff: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/restart.png", {
-        restart: [0,0]
-    });
-    // подгружаем спрайт
-    Crafty.sprite(Settings.poligon, "images/ground.png", {
-        ground: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/sand.jpg", {
-        sand: [0,0]
-    });
-    // подгружаем спрайт
-    Crafty.sprite(Settings.poligon, "images/cargo1.png", {
-        cargo1: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/house.png", {
-        house: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/track.png", {
-        track: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/trackfull.png", {
-        trackfull: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/trackTarget.png", {
-        tracktarget: [0,0]
-    });
-    Crafty.sprite(Settings.poligon, "images/trackfulltarget_completed.png", {
-        trackfulltargetcompleted: [0,0]
-    });
+
+    sprites = {
+        digger: "images/digger.png",
+        ground: "images/ground.png",
+        sand: "images/sand.jpg",
+        house: "images/house.png",
+
+        soundon: "images/soundon.png",
+        soundoff: "images/soundoff.png",
+        restart: "images/restart.png",
+
+        target1: "images/target1.png",
+        cargo1: "images/cargo1.png",
+        target1completed: "images/target1_completed.png",
+
+        monster: "images/monster.png",
+
+        track: "images/track.png",
+        tracktarget: "images/trackTarget.png",
+        tracktargetcompleted: "images/tracktarget_completed.png",
+        trackfull: "images/trackfull.png",
+        trackfulltargetcompleted: "images/trackfulltarget_completed.png",
+    }
+
+    for (var key in sprites) {
+        console.log("Load sprite", key, sprites[key] );
+        // подгружаем спрайт
+        attr = {}
+        attr[key] = [0, 0]
+        Crafty.sprite(Settings.poligon, sprites[key], attr);
+    }
+
+    sounds = {
+        tractor: "sounds/tractor.wav",
+        money: "sounds/money.wav",
+        namnam: "sounds/burp.wav",
+        water: "sounds/water.wav",
+        hit: "sounds/hit.wav",
+        laught: "sounds/laught.wav",
+        deliver2target: "sounds/deliver2target.wav",
+        deliver2target2: "sounds/deliver2target2.wav",
+        track_go2target: "sounds/track_go2target.wav",
+        trackfull_go2target: "sounds/trackfull_go2target.wav",
+    }
 
     var isPhoneGapUse = false;
-    if (isPhoneGapUse) {
-        Game.sounds.tractor = new Media("/android_asset/www/sounds/tractor.wav")
-        Game.sounds.money = new Media("/android_asset/www/sounds/money.wav")
-        Game.sounds.namnam = new Media("/android_asset/www/sounds/burp.wav")
-        Game.sounds.water = new Media("/android_asset/www/sounds/water.wav")
-        Game.sounds.hit = new Media("/android_asset/www/sounds/hit.wav")
-        Game.sounds.laught = new Media("/android_asset/www/sounds/laught.wav")
-        Game.sounds.deliver2target = new Media("/android_asset/www/sounds/deliver2target.wav")
-        Game.sounds.deliver2target2 = new Media("/android_asset/www/sounds/deliver2target2.wav")
-        Game.sounds.track_go2target = new Media("/android_asset/www/sounds/track_go2target.wav")
-        Game.sounds.trackfull_go2target = new Media("/android_asset/www/sounds/trackfull_go2target.wav")
-    } else {
-    
-        Crafty.audio.add("tractor", "sounds/tractor.wav")
-        Crafty.audio.add("money", "sounds/money.wav")
-        Crafty.audio.add("burp", "sounds/burp.wav")
-        Crafty.audio.add("water", "sounds/water.wav")
-        Crafty.audio.add("hit", "sounds/hit.wav")
-        Crafty.audio.add("laught", "sounds/laught.wav")
-        Crafty.audio.add("deliver2target", "sounds/deliver2target.wav")
-        Crafty.audio.add("deliver2target2", "sounds/deliver2target2.wav")
-        Crafty.audio.add("track_go2target", "sounds/track_go2target.wav")
-        Crafty.audio.add("trackfull_go2target", "sounds/trackfull_go2target.wav")
-
-        Game.sounds.tractor = {play: function() {
-            Crafty.audio.play("tractor", 1);
-        }};
-        Game.sounds.money = {play: function() {
-            Crafty.audio.play("money", 1);
-        }};
-        Game.sounds.namnam = {play: function() {
-            Crafty.audio.play("burp", 1);
-        }};
-        Game.sounds.water = {play: function() {
-            Crafty.audio.play("water", 1);
-        }};
-        Game.sounds.hit = {play: function() {
-            Crafty.audio.play("hit", 1);
-        }};
-        Game.sounds.laught = {play: function() {
-            Crafty.audio.play("laught", 1);
-        }};
-        Game.sounds.deliver2target2 = {play: function() {
-            Crafty.audio.play("deliver2target2", 1);
-        }};
-        Game.sounds.deliver2target = {play: function() {
-            Crafty.audio.play("deliver2target", 1);
-        }};
-        Game.sounds.track_go2target = {play: function() {
-            Crafty.audio.play("track_go2target", 1);
-        }};
-        Game.sounds.trackfull_go2target = {play: function() {
-            Crafty.audio.play("trackfull_go2target", 1);
-        }};
+    for (var key in sounds) {
+        console.log("Load sound", key, sprites[key]);
+        if (isPhoneGapUse) {
+            Game.sounds[key] = new Media("/android_asset/www/" + sounds[key])
+        } else {
+            Crafty.audio.add(key, sounds[key])
+            Game.sounds[key] = {
+                play: function() {
+                    if (Settings.sound)
+                        Crafty.audio.play(key, 1);
+                }
+            };
+        }
     }
 
     // запускаем первую сцену
